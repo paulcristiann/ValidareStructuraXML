fcc25451e1e49b4cdf2d90bff3cb98788773ada5
Paul-Cristian Vasile <vasilepaul97@gmail.com>
2018-05-13 19:10:41 +0300

    Update

--- src/main/Main.java
+++ src/main/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("validate.fxml"));
         primaryStage.setTitle("Validare Structura XML");
-        primaryStage.setScene(new Scene(root, 800, 600));
+        primaryStage.setScene(new Scene(root, 800, 400));
         primaryStage.show();
     }
 
